3a8648bcd4c9642699e8afdaa3f3bac5dd74cd0f
Kavita <bhardwajkavita019@gmail.com>
2020-01-24 16:42:15 -0500

    Completed Lab2

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            string[] products = { "Laptop","Tablet","Desktop"};
+            ViewData["products"] = products;
+            return View();
+        }
+
+        public String Details(string product)
+        {
+            string message = "You selected product: " + product;
+            return message;
+        }
+    }
+}
